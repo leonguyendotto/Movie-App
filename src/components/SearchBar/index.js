3c67d9627c12a8abe6f0b844f4790dc72c9130a1
leonguyendotto <leonguyen.to@gmail.com>
2022-06-08 09:52:38 -0400

    Initial commit

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,42 @@
+import React, {useEffect,useState, useRef} from "react";
+
+//Image
+import searchIcon from '../../images/search-icon.svg';
+
+//Styles 
+import { Wrapper,Content } from "./SearcBar.styles";
+
+const SearchBar = ({setSearchTerm}) => {
+
+    const [state, setState]= useState('');
+    const initial = useRef(true);
+
+    useEffect(()=> {
+        if(initial.current){
+            initial.current=false;
+            return;
+        }
+
+        const timer = setTimeout(()=> {
+            setSearchTerm(state);
+        }, 500)
+
+        return () => clearTimeout(timer)
+    }, [setSearchTerm, state])
+
+    return (
+        <Wrapper>
+            <Content>
+                <img src={searchIcon} alt='search-icon'/>
+                <input
+                    type='text'
+                    placeholder="Search Movie"
+                    onChange={e => setState(e.currentTarget.value)}
+                    value={state}
+                />
+            </Content>
+        </Wrapper>
+    )
+};
+
+export default SearchBar;
