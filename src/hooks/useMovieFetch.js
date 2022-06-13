3d11e6166ffc0152fc74edd49c35529ee347579b
leonguyendotto <leonguyen.to@gmail.com>
2022-06-13 13:16:57 -0400

    Minor fix

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -17,8 +17,6 @@ export const useMovieFetch = movieId => {
                 const movie = await API.fetchMovie(movieId);
                 const credits = await API.fetchCredits(movieId);
 
-                console.log(movie);
-
                 // Get directors only
                 const directors = credits.crew.filter (
                     member => member.job === 'Director'
